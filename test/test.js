0a65de0810f03a26a23bbe6d98f8ffe695a3da12
Domenic Denicola <domenic@domenicdenicola.com>
2013-08-15 20:23:35 -0400

    Redo tests to test actual results, not JS output.
    This makes them less brittle in the face of browserify format changes.

--- test/test.js
+++ test/test.js
@@ -3,10 +3,17 @@
 var path = require("path");
 var fs = require("fs");
 var assert = require("assert");
+var jsdom = require("jsdom");
 
-specify("It works", function () {
-    var output = fs.readFileSync(path.resolve(__dirname, "output/output.js"), "utf-8");
-    var expected = fs.readFileSync(path.resolve(__dirname, "expected/expected.js"), "utf-8");
-
-    assert.strictEqual(output, expected);
+specify("It works", function (done) {
+    jsdom.env({
+        html: "<!DOCTYPE html><html><head><title>Grunt Global Wrap Test</title></head><body></body></html>",
+        scripts: [path.resolve(__dirname, "output/output.js")],
+        src: ["window.theTestGlobal();"],
+        done: function (err, window) {
+            assert.ifError(err);
+            assert.strictEqual(window.testData, "beep boop; typeof process is: undefined");
+            done();
+        }
+    });
 });
